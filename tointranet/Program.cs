5361b2ed564a4fa0b74ed0e7a9117d60b6318a4c
hedulewei <yang_clin@aliyun.com>
2017-09-10 09:33:38 +0800

    export ok

--- tointranet/Program.cs
+++ tointranet/Program.cs
@@ -8,8 +8,8 @@ namespace exportdb
 {
     class Program
     {
-        static string dbtofilePath = "/home/driverbusiness/dbtofile";
-        static string exportPath = "/home/driverbusiness/ftp/put";
+        static string dbtofilePath = "dbtofile";
+        static string exportPath = "ftp/put";
         public enum businessType
         {
             unknown,
@@ -33,6 +33,27 @@ namespace exportdb
 , changeContact//变更联系方式
 
         };
+
+        public enum picType
+        {
+            unknown,
+            id_front,//身份证正面
+            id_back,//身份证反面
+            id_inhand, delay_pic, driver,
+
+            health,//机动车驾驶人身体条件证明
+            overage, expire,
+            hukou_pic, //户口簿本人信息变更页
+            sign_pic, //驾驶证遗失声明
+            declaration_sign,//申告义务签名
+
+            face1,//人脸识别拍照一
+            face2,//人脸识别拍照二
+            face3,
+            passport//护照
+      , visa//签证
+      , serviceNote//入伍通知书
+        };
         public static class global
         {
             public static Dictionary<businessType, short> businesscount = new Dictionary<businessType, short>();
@@ -45,7 +66,7 @@ namespace exportdb
                 businesscount.Add(businessType.delay, 6);//延期换证?
                 businesscount.Add(businessType.lost, 6);//遗失补证
                 businesscount.Add(businessType.damage, 5);//损毁换证
-                businesscount.Add(businessType.overage, 6);//超龄换证?
+                businesscount.Add(businessType.overage, 6);//超龄换证?67
 
                 businesscount.Add(businessType.expire, 6);//期满换证??6,7??
                 businesscount.Add(businessType.changeaddr, 5);//变更户籍地址
@@ -74,8 +95,10 @@ namespace exportdb
             var dir = string.Format("{0}-{1}-{2}-{3}-{4}-{5}", date.Year, date.Month.ToString("D2"), date.Day.ToString("D2"),
             date.Hour.ToString("D2"), date.Minute.ToString("D2"), date.Second.ToString("D2"));
             var dbtofilefname = dir + "-abo.txt";
-            if (!Directory.Exists(dbtofilePath)) Directory.CreateDirectory(dbtofilePath);
-            var fname = Path.Combine(dbtofilePath, dbtofilefname);
+            var home = Environment.GetEnvironmentVariable("HOME");
+            var dbtofp = Path.Combine(home, dbtofilePath);
+            if (!Directory.Exists(dbtofp)) Directory.CreateDirectory(dbtofp);
+            var fname = Path.Combine(dbtofp, dbtofilefname);
             using (var db = new blahContext())
             {
                 var theuser = db.Business.Where(async => async.Integrated == false);
@@ -86,41 +109,65 @@ namespace exportdb
 
                     if (pics.Count() >= global.businesscount[(businessType)re.Businesstype])
                     {
-                        NewMethod(re.Identity, re.Businesstype.ToString());
+                        var bt = (businessType)re.Businesstype;
+                        if (bt == businessType.delay || bt == businessType.overage || bt == businessType.expire)
+                        {
+                            if (!checkSignpic(bt, re.Identity, home)) continue;
+                        }
+                        NewMethod(re.Identity, bt.ToString(), home);
                         var phone = string.Empty;
                         if (re.Businesstype == (short)businessType.changeContact)
                         {
-                            var userp=db.Aouser.FirstOrDefault(h =>h.Identity==re.Identity);
-                            if(userp!=null){
-                                phone=userp.Phone;
+                            var userp = db.Aouser.FirstOrDefault(h => h.Identity == re.Identity);
+                            if (userp != null)
+                            {
+                                phone = userp.Phone;
                             }
                         }
                         var line = string.Format("{0},{1},{2},{3},{4},{5}",
-                         re.Identity, re.Businesstype, re.Postaddr, re.Acceptingplace, re.QuasiDrivingLicense,phone);
+                         re.Identity, re.Businesstype, re.Postaddr, re.Acceptingplace, re.QuasiDrivingLicense, phone);
                         File.AppendAllText(fname, line + "\r\n");
+                        re.Integrated = true;
                     }
-                    re.Integrated = true;
+
                 }
                 db.SaveChanges();
             }
-            if (!Directory.Exists(exportPath)) Directory.CreateDirectory(exportPath);
-            var zipfname = Path.Combine(exportPath, dir+"-abo");
+            var exp = Path.Combine(home, exportPath);
+            if (!Directory.Exists(exp)) Directory.CreateDirectory(exp);
+            var zipfname = Path.Combine(exp, dir + "-abo");
 
             var a = new System.Diagnostics.Process();
             a.StartInfo.UseShellExecute = true;
             a.StartInfo.Arguments =
-            string.Format(" {0} {1}/* -r", zipfname, dbtofilePath);
+            string.Format(" {0} {1}/* -r", zipfname, dbtofp);
             a.StartInfo.FileName = "zip";
             a.Start();
             a.WaitForExit();
         }
 
-        private static void NewMethod(string filebase, string btype)
+        private static bool checkSignpic(businessType bt, string id, string home)
         {
+            var picp = string.Format("{2}/server/pictures/{0}/{1}", id, bt.ToString(), home);
+            if (!Directory.Exists(picp)) return false;
+            var pp = new DirectoryInfo(picp).GetFiles();
+            foreach (var ff in pp)
+            {
+                if (ff.Name.Contains(picType.declaration_sign.ToString())) return true;
+            }
+            return false;
+        }
+
+        private static void NewMethod(string filebase, string btype, string home)
+        {
+            var  bpath=Path.Combine(home,dbtofilePath,filebase);
+             if(!Directory.Exists(bpath)) Directory.CreateDirectory(bpath);
+             
             var a = new System.Diagnostics.Process();
             a.StartInfo.UseShellExecute = true;
             a.StartInfo.Arguments =
-            string.Format(" -r /home/server/picures/{1}/{2}/* {0}/", dbtofilePath, filebase, btype);
+           
+            string.Format(" -r {3}/publish/pictures/{1}/{2} {3}/{0}/{1}/", dbtofilePath, filebase, btype, home);
             a.StartInfo.FileName = "cp";
             a.Start();
             a.WaitForExit();
